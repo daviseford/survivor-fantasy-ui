e6ca670cd795d058d5d353733e95394c68be92bc
Davis Ford <daviseford@users.noreply.github.com>
2024-02-13 09:28:56 -0700

    Initial firebase commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { getApps } from "firebase/app";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+
+  const firebaseApps = getApps();
 
   return (
     <>
@@ -17,6 +20,11 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
+
+      <code>
+        <pre>{JSON.stringify(firebaseApps, null, 2)}</pre>
+      </code>
+
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -29,7 +37,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,33 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// https://dev.to/farazamiruddin/react-firebase-add-firebase-to-a-react-app-4nc9
+const firebaseConfig = {
+  apiKey: "AIzaSyB1T5LgmsCrKQHIIeMokbglUBWduiksD_Y",
+  authDomain: "survivor-fantasy-51c4b.firebaseapp.com",
+  projectId: "survivor-fantasy-51c4b",
+  storageBucket: "survivor-fantasy-51c4b.appspot.com",
+  messagingSenderId: "608839041450",
+  appId: "1:608839041450:web:0bc074ccf72ad23ff4ebd3",
+  measurementId: "G-W4DJMFJDPW",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+getAnalytics(app);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
