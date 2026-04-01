28a473ca5e0195029f069d02ef8ecc3cf9b680ef
Davis Ford <daviseford@users.noreply.github.com>
2026-03-31 18:28:50 -0600

    feat: add Season 49 as a playable season
    Full season data with 18 players (including Shannon Fairweather),
    13 episodes, 25 challenges, 17 eliminations, and 15 game events.
    Player images auto-resolved from wiki. Season logo sourced from wiki.
    Also documents yarn new-season as the preferred season bootstrap command.

--- src/data/seasons.ts
+++ src/data/seasons.ts
@@ -1,5 +1,6 @@
 import { Season } from "../types";
 import { SEASON_46_EPISODES, SEASON_46_PLAYERS } from "./season_46";
+import { SEASON_49_EPISODES, SEASON_49_PLAYERS } from "./season_49";
 import { SEASON_50_EPISODES, SEASON_50_PLAYERS } from "./season_50";
 import { SEASON_9_EPISODES, SEASON_9_PLAYERS } from "./season_9";
 
@@ -22,6 +23,16 @@ export const SEASONS = {
 
     players: SEASON_46_PLAYERS,
     episodes: SEASON_46_EPISODES,
+  },
+
+  season_49: {
+    id: "season_49",
+    order: 49,
+    name: "Survivor 49",
+    img: "https://static.wikia.nocookie.net/survivor/images/7/70/US_S49_logo.png/revision/latest?cb=20250523231159",
+
+    players: SEASON_49_PLAYERS,
+    episodes: SEASON_49_EPISODES,
   },
 
   season_50: {
